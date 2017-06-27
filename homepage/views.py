e47ae5b0e785fdd4324e115428d0344e631126d1
Christos Spyropoulos <xristosandroid@gmail.com>
2017-06-27 13:55:47 +0200

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -6,11 +6,6 @@ from homepage.models import Tattoo
 
 # Create your views here.
 def home(request):
-    import os
-    # for file in os.listdir("C:/Users/Christos/Desktop/digitalocean/Django_Digitalocean/static/images"):
-    #     if file.endswith(".jpg"):
-    #         b = Tattoo(tattoo_type='oldschool',tattoo_color='black',tattoo_place='body',tattoo_photo=os.path.join("", file))
-    #         b.save()
     tattoo_list = Tattoo.objects.all()
     args = {"tattoos" : tattoo_list}
     return render(request, 'homepage/homepage.html', args)
